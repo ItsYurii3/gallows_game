721ad290a6af5fd45f4e58e46c50c55f7011df72
Yurii Lukirin <medium12345q@gmail.com>
2025-03-06 10:32:50 +0100

    add: implement class Player

---
+++ player.py
@@ -0,0 +1,26 @@
+class Player:
+    
+    def __init__(self, name):
+        self.name = name
+        self.attempt_limit = 6
+        self.attempt_user = 0
+    
+    def enter_char(self, word: str):
+        while(True):
+            ch = input('').strip().lower()
+            if len(ch) == 1 and ch.isalpha():
+                if ch in word:
+                    print("You have guessed the character")
+                    return ch, True
+                else:
+                    print("You don`t guessed the character")
+                    self.attempt_user += 1
+                    return ch, False
+            else:
+                print("You have entered not correct symbol")    
+            
+         
+            
+        
+    def check_attempt_limit(self):
+        return self.attempt_limit == self.attempt_user
